eee8fad793a27fe33529b2fe01ae3fc7a508d5ae
Arthur Teixeira <arthursony1@hotmail.com>
2023-02-28 15:03:45 -0300

    requisito 1 concluido

--- src/average.js
+++ src/average.js
@@ -11,6 +11,20 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  if (array.length === 0) {
+    return undefined;
+  }
+  let num = 0;
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] !== 'number') {
+      return undefined;
+    }
+    num += array[i];
+  }
+  let media = num / array.length;
+  let resultado = media.toFixed();
+  return parseInt(resultado, 10);
+};
 
 module.exports = average;
